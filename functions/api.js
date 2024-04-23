7bdc5d420198bb67951774142dacb0077aa2b10c
Ian Reniel R. Rey <ianrenielrey25@gmail.com>
2024-04-23 22:42:28 +0800

    hey

--- functions/api.js
+++ functions/api.js
@@ -7,7 +7,7 @@ const cors = require ('cors');
 
 const app = express();
 
-const dbCloudUrl= 'mongodb+srv://renielian03:IanRey23@cluster0.l3hgyiz.mongodb.net/';
+const dbCloudUrl= 'mongodb+srv://renielian03:IanRey23@cluster0.l3hgyiz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
 
 app.use(cors());
